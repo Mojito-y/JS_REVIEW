5cbd74b4a34c479a0c53217046ca1bde652f1008
Mojito-y <905843096@qq.com>
2022-08-15 11:31:47 +0800

    Nodejs day04 start

--- Node.js/day03/07.使用express创建最基本的服务器.js
+++ Node.js/day03/07.使用express创建最基本的服务器.js
@@ -4,28 +4,28 @@ const express = require('express')
 const app = express()
 
 // 4. 监听客户端的 GET 和 POST 请求，并向客户端响应具体的内容
-app.get('/user', (req, res) => {
-  // 调用 express 提供的 res.send() 方法，向客户端响应一个 JSON 对象
-  res.send({ name: 'zs', age: 20, gender: '男' })
+app.get('/user',(req,res) => {
+  // 向客户端发送 JSON 对象
+  res.send({name:'周杰伦',age:18,gender:'男'})
 })
-app.post('/user', (req, res) => {
-  // 调用 express 提供的 res.send() 方法，向客户端响应一个 文本字符串
+app.post('/user',(req,res) => {
   res.send('请求成功')
 })
-app.get('/', (req, res) => {
+app.get('/',(req,res) => {
   // 通过 req.query 可以获取到客户端发送过来的 查询参数
-  // 注意：默认情况下，req.query 是一个空对象
-  console.log(req.query)
+  // 注意： 默认情况下， req.query 是个空对象
+  console.log(req.query);
   res.send(req.query)
+  
 })
 // 注意：这里的 :id 是一个动态的参数
-app.get('/user/:ids/:username', (req, res) => {
-  // req.params 是动态匹配到的 URL 参数，默认也是一个空对象
-  console.log(req.params)
+app.get('/user/:ids/:name',(req,res) => {
+  // req.params 是动态匹配到的url参数，默认也是个空对象
+  console.log(req.params);
   res.send(req.params)
 })
 
 // 3. 启动 web 服务器
-app.listen(80, () => {
-  console.log('express server running at http://127.0.0.1')
+app.listen(8081,() => {
+  console.log('express server running at http://127.0.0.1:8081');
 })
--- Node.js/day03/08.使用express.static对外提供静态资源.js
+++ Node.js/day03/08.使用express.static对外提供静态资源.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
-// 在这里，调用 express.static() 方法，快速的对外提供静态资源
-app.use('/files', express.static('./files'))
+// 调用express.static()方法，快速的对外提供静态资源
 app.use(express.static('./clock'))
+app.use(express.static('./files'))
 
-app.listen(80, () => {
-  console.log('express server running at http://127.0.0.1')
+
+
+app.listen(8081,() => {
+  console.log('express server running at http://127.0.0.1:8081');
 })
