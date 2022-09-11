fa2037b1fb303855dd14f4afa679f342910c6408
Mojito-y <905843096@qq.com>
2022-09-11 19:34:54 +0800

    vue day5 middle

--- Vue2/day5/demo-1/src/App.vue
+++ Vue2/day5/demo-1/src/App.vue
@@ -1,16 +1,59 @@
 <template>
   <div class="app-container">
-    <h1>App 根组件</h1>
+    <h1 ref="myh1">App 根组件</h1>
+    <button @click="showThis">打印 this</button>
+    <button @click="onReset">重置Left组件的count值为0</button>
     <hr />
+
+    <input type="text" v-if="inputVisible" @blur="showButton" ref="input">
+    <button v-else @click="showInput">展示输入框</button>
+    <hr>
 
     <div class="box">
       <!-- 渲染 Left 组件和 Right 组件 -->
+      <Left ref="comLeft"></Left>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Left from './components/Left.vue'
+export default {
+  data () {
+    return {
+      // 控制输入框和按钮的按需切换
+      // 默认值为false 表示默认展示按钮 隐藏输入框
+      inputVisible:false,  
+    }
+  },
+  methods: {
+    showThis(){
+      console.log(this);
+      // console.log(this.$refs.myh1);
+      this.$refs.myh1.style.color = 'pink'
+    },
+    // 点击按钮 重置left组件的count
+    onReset(){
+      this.$refs.comLeft.resetCount();
+    },
+    // 点击按钮展示输入框
+    showInput(){
+      // 1.切换布尔值 展示文本框
+      this.inputVisible = true;
+      // 2.让展示出来的文本框 自动获取焦点
+      this.$nextTick(() => {
+        this.$refs.input.focus();
+      })
+    },
+    // 输入框失去焦点时隐藏输入框 显示按钮
+    showButton(){
+      this.inputVisible = false
+    }
+  },
+  components: {
+    Left
+  }
+}
 </script>
 
 <style lang="less">
---
+++ Vue2/day5/demo-cart/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+    <!-- Header 头部区域 -->
+    <Header></Header>
+    <h1>App 根组件</h1>
+  </div>
+</template>
+
+<script>
+// 导入需要的组件
+import Header from './components/Header/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.app-container {
+  padding-top: 45px;
+  padding-bottom: 50px;
+}
+</style>
